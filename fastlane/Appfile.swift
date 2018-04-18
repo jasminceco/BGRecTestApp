d914a655ee26e6a4bd3f38fba12ed65bb4b2546e
Jasmin Ceco <jasminceco@jasmincecomac.local>
2018-04-18 13:36:41 +0200

    removed

--- fastlane/Appfile.swift
+++
@@ -1,9 +0,0 @@
-var appIdentifier: String { return "com.comtrade.BGRecordingTestApp" } // The bundle identifier of your app
-var appleID: String { return "jasmin.ceco@gmail.com" } // Your Apple email address
-
-var itcTeam: String? { return "118104269" } // iTunes Connect Team ID
-var teamID: String { return "HMQALEF4UR" } // Apple Developer Portal Team ID
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
